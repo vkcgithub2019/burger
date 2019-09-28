327b7cdfd81e54b832b1b8ee6190911ce7d3aa61
Victor Chung <victochung998@yahoo.com>
2019-09-28 13:37:16 -0700

    got the app to work but need to finish up

---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO burgers (burger_name) VALUES ('Deluxe Cheeseburger'), ('Gourmet Burger'), ('Mushroom Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', true), ('Whoopy Hamburger', false), ('Guacamole Burger', true);
