004e91cc27407105d52b069ecd80429379ae295d
Victor Chung <victochung998@yahoo.com>
2019-09-30 14:39:09 -0700

    getting the app to work in heroku

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    /* port: 3306, */
+    port: 3306,
     host: "localhost",
     user: "root",
     password: "",
